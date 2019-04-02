6de55db3e669666beb2975ec5ed8ba7049fc022a
jamiemills98 <jamiemills98@hotmail.com>
2019-04-02 14:21:01 +1000

    Arrays Challenge answer

---
+++ ArraysChallenge.rb
@@ -0,0 +1,30 @@
+#four_letter_animals Array
+four_letter_animals = ["calf", "duck", "elephant", "goat", "lamb","lion","mule", "dog"]
+
+#add puma to the end
+    four_letter_animals.push("puma")
+        puts four_letter_animals
+
+#add joey to index 4
+   four_letter_animals.insert(4,"Joey")
+        puts four_letter_animals
+    
+#delete dog
+    four_letter_animals.delete("dog")
+        puts four_letter_animals
+
+#reverse order
+    four_letter_animals.reverse
+        puts four_letter_animals
+
+#replace elephant with foal
+    four_letter_animals[2]= "foal"
+        puts four_letter_animals
+
+#add bear on the end
+    four_letter_animals.push("bear")
+        puts four_letter_animals
+
+#reverse again
+    four_letter_animals.reverse
+        puts four_letter_animals
