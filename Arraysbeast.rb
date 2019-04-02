1b22788e8d7ddf26c23d4d5593253f26755ecb6b
jamiemills98 <jamiemills98@hotmail.com>
2019-04-02 15:12:41 +1000

    updated answer

---
+++ Arraysbeast.rb
@@ -0,0 +1,22 @@
+# Ask the user for three or more grocery items
+groceries=[]
+#program is run and outputs:
+
+3.times do 
+    #groceries
+
+puts "Please enter Grocery item."
+    item = gets.chomp
+    groceries << item
+end 
+#grocery listed back 
+puts groceries.sort
+puts groceries.length
+
+
+
+
+
+
+
+
